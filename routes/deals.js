7fc956d0c95029a4eb25eaa60a6df6915db77cb1
Patryk Ostrowski <ostrowski.pat@gmail.com>
2026-01-17 12:04:09 +0100

    Zadanie z zamina strony deals na album

--- routes/deals.js
+++ routes/deals.js
@@ -1,30 +1,60 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Deal = require('../models/Deal');
+const Deal = require("../models/Deal");
 
 // GET All Deals
-router.get('/', async (req, res) => {
-    try {
-        const deals = await Deal.find({});
-        res.render('deals/index', { title: 'Deals', deals, layout: './Layouts/layout' });
-    } catch (err) {
-        console.error(err);
-        res.status(500).send('Server Error');
-    }
+router.get("/", async (req, res) => {
+  try {
+    const deals = await Deal.find({});
+    res.render("deals/index", {
+      title: "Deals",
+      deals,
+      layout: "./Layouts/layout",
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server Error");
+  }
 });
 
 // GET Deal Details
-router.get('/:id', async (req, res) => {
-    try {
-        const deal = await Deal.findById(req.params.id);
-        if (!deal) {
-            return res.status(404).send('Deal not found');
-        }
-        res.render('deals/show', { title: deal.title, deal, layout: './Layouts/layout' });
-    } catch (err) {
-        console.error(err);
-        res.status(500).send('Server Error');
+router.get("/:id", async (req, res) => {
+  try {
+    const deal = await Deal.findById(req.params.id);
+    if (!deal) {
+      return res.status(404).send("Deal not found");
     }
+    res.render("deals/show", {
+      title: deal.title,
+      deal,
+      layout: "./Layouts/layout",
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server Error");
+  }
+});
+
+// POST Upvote Deal
+router.post("/:id/upvote", async (req, res) => {
+  try {
+    await Deal.findByIdAndUpdate(req.params.id, { $inc: { score: 1 } });
+    res.redirect("/deals");
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server Error");
+  }
+});
+
+// POST Downvote Deal
+router.post("/:id/downvote", async (req, res) => {
+  try {
+    await Deal.findByIdAndUpdate(req.params.id, { $inc: { score: -1 } });
+    res.redirect("/deals");
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server Error");
+  }
 });
 
 module.exports = router;
