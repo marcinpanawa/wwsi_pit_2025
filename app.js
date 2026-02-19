9cc92c0e92c57e551892225db5152b2384ae88fe
Maryna Afanasyeva <elvawiel@gmail.com>
2026-02-19 21:08:32 +0100

    Add Tips page

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 const authRouter = require("./routes/auth");
 const dealsRouter = require("./routes/deals");
+const tipsRouter = require("./routes/tips");
 
 const app = express();
 
@@ -49,6 +50,7 @@ app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/auth", authRouter);
 app.use("/deals", dealsRouter);
+app.use("/tips", tipsRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
